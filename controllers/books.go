3c56dcfa6658ae6a50450674181abb3866065277
Jan Vaehsen <jvaehsen@deloitte.de>
2023-06-25 20:11:48 +0200

    little more changes

--- controllers/books.go
+++ controllers/books.go
@@ -85,7 +85,7 @@ func DeleteBook(c *gin.Context) {
 	// Get model if exist
 	var book models.Book
 	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "Record not found!"})
 		return
 	}
 
--- models/setup.go
+++ models/setup.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"log"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
@@ -11,6 +13,7 @@ func ConnectDatabase() {
 	database, err := gorm.Open("sqlite3", "test.db")
 
 	if err != nil {
+		log.Fatal(err.Error())
 		panic("Failed to connect to database!")
 	}
 
